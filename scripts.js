cc03d99c4fcf27dac5aa2bc0df48c36b6c261e2d
floretn <ivanse99@yandex.ru>
2022-04-25 20:53:03 +0300

    хы-хы-хы

--- scripts.js
+++ scripts.js
@@ -1,84 +1,83 @@
     const trollRadius = 150;
     const clientWidth = document.documentElement.clientWidth;
     const clientHeight = document.documentElement.clientHeight;
-    let trollLeftX = (clientWidth / 2);
-    let trollTopY = (clientHeight / 2);
-    let trollCenterX = trollLeftX - (trollRadius / 2);
-    let trollCenterY = trollTopY - (trollRadius / 2);
-    let badTrollCenterX;
-    let badTrollCenterY;
+
+    let troll = {
+        name: "troll"
+    };
+
+    let badTroll = {
+        name: "bad_troll"
+    };
+
+    let trolls = [troll, badTroll];
 
     /*
-     * Easy: 250
-     * Medium: 200
-     * Hard: 100
+     * Easy: 200
+     * Medium: 150
+     * Hard: 50
      * Unreal: 0
      */
-    const pause = 250;
+    const pause = 150;
 
     function win(){
         alert('Ты победил!');
-        setStartPosition(document.getElementById('troll'));
+        setStartPosition();
     }
 
-    function setStartPosition(troll) {
-        trollCenterX = (clientWidth / 2);
-        trollCenterY = (clientHeight / 2);
-        trollLeftX = trollCenterX - (trollRadius / 2);
-        trollTopY = trollCenterY - (trollRadius / 2);
-        troll.style.top = trollTopY;
-        troll.style.left = trollLeftX;
-        trollCenterX += (trollRadius / 2);
-        trollCenterY += (trollRadius / 2);
+    function loss(){
+        alert('Ты выбрал не ту кнопку:(');
+    }
+
+    function setStartPosition() {
+        trolls.forEach((troll) => {
+            let trollButton = document.getElementById(troll.name);
+            trollButton.style.position = "absolute";
+            trollButton.style.width = trollRadius + "px";
+            trollButton.style.height = trollRadius + "px";
+            troll.trollCenterX = (clientWidth / 2);
+            troll.trollCenterY = (clientHeight / 2);
+            troll.trollLeftX = troll.trollCenterX - (trollRadius / 2);
+            troll.trollTopY = troll.trollCenterY - (trollRadius / 2);
+            console.log(troll);
+            trollButton.style.left = troll.trollLeftX + "px";
+            trollButton.style.top = troll.trollTopY + "px";
+        });
     }
 
     window.onload = function() {
-        let troll = document.getElementById('troll');
-        troll.style.position = "absolute";
-        troll.style.width = trollRadius;
-        troll.style.height = trollRadius;
-        setStartPosition(troll);
+        setStartPosition();
     }
 
     document.onmousemove = function(e) {
-        while ((Math.pow((MouseCoords.getX(e) - trollCenterX), 2)
-                + Math.pow((MouseCoords.getY(e) - trollCenterY), 2))
-                <= Math.pow((trollRadius), 2)) {
-            trollLeftX = Math.random() * clientWidth;
-            if (trollLeftX > clientWidth - trollRadius * 1.5) {
-                trollLeftX -= trollRadius * 1.5;
+        trolls.forEach((troll) => {
+            while ((Math.pow((MouseCoords.getX(e) - troll.trollCenterX), 2)
+                    + Math.pow((MouseCoords.getY(e) - troll.trollCenterY), 2))
+                    <= Math.pow(trollRadius, 2)) {
+                troll.trollLeftX = Math.random() * clientWidth;
+                if (troll.trollLeftX > clientWidth - trollRadius * 1.5) {
+                    troll.trollLeftX -= trollRadius * 1.5;
+                }
+                troll.trollTopY = Math.random() * clientHeight;
+                if (troll.trollTopY > clientHeight - trollRadius * 1.5) {
+                    troll.trollTopY -= trollRadius * 1.5;
+                }
+                let trollForLambda = document.getElementById(troll.name);
+                setTimeout(() => {
+                    trollForLambda.style.left = troll.trollLeftX + "px";
+                    trollForLambda.style.top = troll.trollTopY + "px";
+                }, pause);
+                troll.trollCenterX = troll.trollLeftX + (trollRadius / 2);
+                troll.trollCenterY = troll.trollTopY + (trollRadius / 2);
             }
-            trollTopY = Math.random() * clientHeight;
-            if (trollTopY > clientHeight - trollRadius * 1.5) {
-                trollTopY -= trollRadius * 1.5;
-            }
-            let trollForLambda = document.getElementById('troll');
-            setTimeout(() => {
-                trollForLambda.style.left = trollLeftX;
-                trollForLambda.style.top = trollTopY;
-            }, pause);
-            trollCenterX = trollLeftX + (trollRadius / 2);
-            trollCenterY = trollTopY + (trollRadius / 2);
-        }
+        });
     }
 
     const MouseCoords = {
-        // X-координата
         getX: function (e) {
-            if (e.pageX) {
-                return e.pageX;
-            } else if (e.clientX) {
-                return e.clientX + (document.documentElement.scrollLeft || document.body.scrollLeft) - document.documentElement.clientLeft;
-            }
-            return 0;
+            return e.pageX;
         },
-        // Y-координата
         getY: function (e) {
-            if (e.pageY) {
-                return e.pageY;
-            } else if (e.clientY) {
-                return e.clientY + (document.documentElement.scrollTop || document.body.scrollTop) - document.documentElement.clientTop;
-            }
-            return 0;
+            return e.pageY;
         }
     };
